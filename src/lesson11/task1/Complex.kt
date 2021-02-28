97f1bdfbe19be02a5e833553336e9d4776ef0859
lidia <lidiapshenichnay@yandex.ru>
2021-02-28 22:03:52 +0300

    lesson 11 (Complex), 12 (PhoneBook)

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,6 +2,8 @@
 
 package lesson11.task1
 
+import lesson1.task1.sqr
+
 /**
  * Класс "комплексное число".
  *
@@ -16,45 +18,105 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(TODO(), TODO())
+    companion object {
+        private fun checkIm(s: String): Double {
+            return if (s == "i") 1.0
+            else s.dropLast(1).toDouble()
+        }
+
+        fun splitForReIm(s: String): Pair<Double, Double> {
+            if (s == "") return 0.0 to 0.0
+            var plus = 0
+            var minus = 0
+            for (simbol in s) {
+                if (simbol == '+') plus++
+                if (simbol == '-') minus++
+            }
+            val list: List<String>
+            when {
+                plus == 1 -> {
+                    list = s.split('+')
+                    return list[0].toDouble() to checkIm(list[1])
+                }
+                minus == 2 -> {
+                    list = s.split('-')
+                    return (-1) * list[1].toDouble() to (-1) * checkIm(list[2])
+                }
+                minus == 1 -> {
+                    if (s.last() != 'i') return s.toDouble() to 0.0
+                    list = s.split('-')
+                    if (list[0] == "") return 0.0 to (-1) * checkIm(list[1])
+                    return list[0].toDouble() to (-1) * checkIm(list[1])
+                }
+                else -> {
+                    if (s.last() != 'i') return s.toDouble() to 0.0
+                    if (s == "i") return 0.0 to 1.0
+                    return 0.0 to s.dropLast(1).toDouble()
+                }
+            }
+        }
+    }
+
+    constructor(s: String) : this(splitForReIm(s).first, splitForReIm(s).second)
+
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(other.re + this.re, this.im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-this.im, -this.re)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(this.re - other.re, this.im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex =
+        Complex(this.re * other.re - this.im * this.im + this.re * other.im + other.re * this.im)
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex {
+        val de = other.re * other.re + other.im * other.im
+        return Complex(
+            (this.re * other.re + this.im * other.im) /
+                    de, (this.im * other.re - this.re * other.im) / de
+        )
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Complex) return false
+        return re == other.re && im == other.im
+    }
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String =
+        when {
+            im == 0.0 && re == 0.0 -> "0.0"
+            re == 0.0 -> "${im}i"
+            im == 0.0 -> "$re"
+            im == 1.0 -> "${re}+i"
+            im == -1.0 -> "${re}-i"
+            else -> if (im > 0) "${re}+${im}i" else "${re}${im}i"
+        }
+
 }
--- src/lesson12/task1/PhoneBook.kt
+++ src/lesson12/task1/PhoneBook.kt
@@ -18,13 +18,15 @@ package lesson12.task1
  * Класс должен иметь конструктор по умолчанию (без параметров).
  */
 class PhoneBook {
+    val pb = mutableMapOf<String, MutableSet<String>>()
+
     /**
      * Добавить человека.
      * Возвращает true, если человек был успешно добавлен,
      * и false, если человек с таким именем уже был в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun addHuman(name: String): Boolean = TODO()
+    fun addHuman(name: String): Boolean = pb.putIfAbsent(name, mutableSetOf()) == null
 
     /**
      * Убрать человека.
@@ -32,7 +34,7 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun removeHuman(name: String): Boolean = TODO()
+    fun removeHuman(name: String): Boolean = pb.remove(name) != null
 
     /**
      * Добавить номер телефона.
@@ -41,7 +43,13 @@ class PhoneBook {
      * либо у него уже был такой номер телефона,
      * либо такой номер телефона зарегистрирован за другим человеком.
      */
-    fun addPhone(name: String, phone: String): Boolean = TODO()
+    fun addPhone(name: String, phone: String): Boolean {
+        val pbName = pb[name]
+        if (pbName == null || humanByPhone(phone) != null) return false
+        pbName.add(phone)
+        return true
+    }
+
 
     /**
      * Убрать номер телефона.
@@ -49,24 +57,35 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * либо у него не было такого номера телефона.
      */
-    fun removePhone(name: String, phone: String): Boolean = TODO()
+    fun removePhone(name: String, phone: String): Boolean = pb[name]?.remove(phone) ?: false
 
     /**
      * Вернуть все номера телефона заданного человека.
      * Если этого человека нет в книге, вернуть пустой список
      */
-    fun phones(name: String): Set<String> = TODO()
+    fun phones(name: String): Set<String> = pb[name] ?: setOf()
 
     /**
      * Вернуть имя человека по заданному номеру телефона.
      * Если такого номера нет в книге, вернуть null.
      */
-    fun humanByPhone(phone: String): String? = TODO()
+    fun humanByPhone(phone: String): String? {
+        for ((name, numberPh) in pb)
+            if (numberPh.contains(phone)) return name
+        return null
+    }
+
 
     /**
      * Две телефонные книги равны, если в них хранится одинаковый набор людей,
      * и каждому человеку соответствует одинаковый набор телефонов.
      * Порядок людей / порядок телефонов в книге не должен иметь значения.
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PhoneBook) return false
+        return this.pb == other.pb
+    }
+
+    override fun hashCode(): Int = pb.hashCode()
 }
